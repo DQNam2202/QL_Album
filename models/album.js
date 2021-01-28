9fcb57b3ca12db76a052897d632274b65eb70a1a
Quốc Nam <duongquocnam224400@gmail.com>
2021-01-28 18:55:59 +0700

    Album

---
+++ models/album.js
@@ -0,0 +1,13 @@
+export class Album {
+  linkAnh = "";
+  moTa = "";
+  theLoai = "";
+  tenAlbum = "";
+  constructor(link, des, theLoai, tenAlbum) {
+    this.linkAnh = link;
+    this.moTa = des;
+    this.theLoai = theLoai;
+    this.tenAlbum = tenAlbum;
+  }
+}
+
